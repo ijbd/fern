24595397a866a3a3ca5d9566d709101ad345455f
ijbd <ijbd@umich.edu>
2022-02-12 17:53:30 -0500

    Finished reorienting axes and simplifying instruction set.

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,224 +1,215 @@
+/* Fern
+ * ~~~~~~~~~~~
+ * Code by ijbd
+ */
+
+// includes
 #include <Servo.h>
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
-// pins
-#define X_DIR_PIN 2
-#define X_STEP_PIN 3
-#define X_MS1 5
-#define X_MS2 4
-#define Y_DIR_PIN 6
-#define Y_STEP_PIN 7
-#define Y_MS1 9
-#define Y_MS2 8
+// defines
+#define MOTOR_A_DIR_PIN 2
+#define MOTOR_A_STEP_PIN 3
+#define MOTOR_A_MS_1_PIN 5
+#define MOTOR_A_MS_2_PIN 4
+#define MOTOR_A_INVERT false
+#define MOTOR_B_DIR_PIN 6
+#define MOTOR_B_STEP_PIN 7
+#define MOTOR_B_MS_1_PIN 9
+#define MOTOR_B_MS_2_PIN 8
+#define MOTOR_B_INVERT false
 #define SERVO_PIN 10
-#define X_BUMP_PIN 11
-#define Y_BUMP_PIN 12
+#define SERVO_ANGLE_DOWN 0
+#define SERVO_ANGLE_UP 30
+#define SERVO_DELAY 1500
+#define BUMPER_X_PIN 11
+#define BUMPER_Y_PIN 12
+#define STEP_MAX_SPEED 600 
+#define STEPS_PER_MM 50 
+#define X_LIM_STEPS 12500
+#define Y_LIM_STEPS 15000
 
-// parameters
-#define INVERT_X_DIR true
-#define INVERT_Y_DIR true
-#define STEPS_PER_100_UM 20 // steps
-#define STEPPER_MAX_SPEED 3000 // steps/sec
-#define SERVO_DELAY 1500 // us
-#define SERVO_ANGLE_UP 30 // deg
-#define SERVO_ANGLE_DOWN 0 // deg
-#define X_MAX 2000 // 100 um
-#define Y_MAX 3000 // 100 um
-#define X_OFFSET 20000 // steps
-#define Y_OFFSET 30000 // steps
-
+// struct
 class Fern{
-  public:
-  Fern(){
-    
+public:
+  Fern():pen(),motor_a(),motor_b(),motors(),pen_state(0){
     // attach servo
     pen.attach(SERVO_PIN);
-    penState = 0;
-    raisePen();
+
+    // init motors
+    init_motors();
+
+    // go home
+    go_home();
+  }
+
+  void init_motors(){
     
     // instantiate motors
-    xMotor = AccelStepper(AccelStepper::DRIVER,X_STEP_PIN,X_DIR_PIN);
-    yMotor = AccelStepper(AccelStepper::DRIVER,Y_STEP_PIN,Y_DIR_PIN);
-
-    // limit speed
-    xMotor.setMaxSpeed(STEPPER_MAX_SPEED);
-    yMotor.setMaxSpeed(STEPPER_MAX_SPEED);
-
-    // invert pins (if necessary)
-    xMotor.setPinsInverted(INVERT_X_DIR);
-    yMotor.setPinsInverted(INVERT_Y_DIR);
-
-    // 1/8 microstepping
-    pinMode(X_MS1,OUTPUT);
-    pinMode(X_MS2,OUTPUT);
-    pinMode(Y_MS1,OUTPUT);
-    pinMode(Y_MS2,OUTPUT);
-    
-    digitalWrite(X_MS1,HIGH);
-    digitalWrite(X_MS2,HIGH);
-    digitalWrite(Y_MS1,HIGH);
-    digitalWrite(Y_MS2,HIGH);
-    
-    // attach motors
-    if(!motors.addStepper(xMotor)){
-      error();
-    }
-    if(!motors.addStepper(yMotor)){
-      error();
-    }     
-
-    // calibration
-    goHome();
-
-    // initialize destination array
-        
-  }
-
-  void raisePen(){
-    if(!penState){
-      pen.write(SERVO_ANGLE_UP);
-      penState = HIGH;
-      delayMicroseconds(SERVO_DELAY);
-    }
-  }
-
-  void lowerPen(){
-    if(penState){
-      pen.write(SERVO_ANGLE_DOWN);
-      penState = LOW;
-      delayMicroseconds(SERVO_DELAY);
-    }
-  }
-
-  void goHome(){
-    raisePen();
-
-    // reset orientation
-    xMotor.setCurrentPosition(0);
-    yMotor.setCurrentPosition(-Y_OFFSET);
-
-    long dest[2] = {-X_OFFSET,-Y_OFFSET};
-
-    // move x axis until bump
-    motors.moveTo(dest);
-    while(digitalRead(X_BUMP_PIN)){
-      if(!motors.run()){
-        xMotor.setCurrentPosition(0);
-        motors.moveTo(dest);
-      }
-    }
-
-    xMotor.setCurrentPosition(-X_OFFSET);
-    delay(1000);
-
-    // move y axis until bump
-    yMotor.setCurrentPosition(0);
-    motors.moveTo(dest);
-    while(digitalRead(Y_BUMP_PIN)){
-      if(!motors.run()){
-        yMotor.setCurrentPosition(0);
-        motors.moveTo(dest);
-      }
-    }
-
-    yMotor.setCurrentPosition(-Y_OFFSET);
-    delay(1000);
-  }
+    motor_a = AccelStepper(AccelStepper::DRIVER,MOTOR_A_STEP_PIN, MOTOR_A_DIR_PIN);
+    motor_b = AccelStepper(AccelStepper::DRIVER,MOTOR_B_STEP_PIN, MOTOR_B_DIR_PIN);
   
-  void movePenTo(int x, int y){
+    // set microstep pins
+    digitalWrite(MOTOR_A_MS_1_PIN, HIGH);
+    digitalWrite(MOTOR_A_MS_2_PIN, HIGH);
+    digitalWrite(MOTOR_B_MS_1_PIN, HIGH);
+    digitalWrite(MOTOR_B_MS_2_PIN, HIGH);
+  
+    // limit speed
+    motor_a.setMaxSpeed(STEP_MAX_SPEED);
+    motor_b.setMaxSpeed(STEP_MAX_SPEED);
+  
+    // invert pins if necessery
+    motor_a.setPinsInverted(MOTOR_A_INVERT);
+    motor_b.setPinsInverted(MOTOR_B_INVERT);
+  
+    // add motor
+    motors.addStepper(motor_a);
+    motors.addStepper(motor_b);
+  }
 
-    // initialize dest
-    long dest[2] = {x,y};
+  void go_home(){
+    
+    // lift 
+    raise_pen();
+    
+    // find x = 0
+    while(digitalRead(BUMPER_X_PIN)){
+      motor_a.setSpeed(-STEP_MAX_SPEED);
+      motor_a.runSpeed();   
+      motor_b.setSpeed(-STEP_MAX_SPEED);
+      motor_b.runSpeed();   
+    }
+    
+    // find y = 0
+    while(digitalRead(BUMPER_Y_PIN)){
+      motor_a.setSpeed(STEP_MAX_SPEED);
+      motor_a.runSpeed();
+      motor_b.setSpeed(-STEP_MAX_SPEED);
+      motor_b.runSpeed();
+    }
+   
+    // set position
+    motor_a.setCurrentPosition(0);
+    motor_b.setCurrentPosition(0);
+  }
+
+  void move_to(int x, int y){
+    
+    // rotate x/y into a/b coordinate system
+    long dest[2];
+    
+    dest[0] = .707*(x-y);
+    dest[1] = .707*(x+y);
+
     motors.moveTo(dest);
     motors.runSpeedToPosition();
   }
 
-  void parseSerial(){
-    /* Instruction set sent in 3 bytes 
-     * bit 0 -> blank
-     * bit 1 -> opcode: 0 for pen raise/lower, 1 for pen move
-     * bits 2-12 -> field 1: null for pen raise/lower, destination x (in mm) / .16 for pen move
-     * bits 13-23 -> field 2: null for pen raise/lower, destination y (in mm) / .16 for pen move
-     */
-
-    Serial.write(1);
-    
-    int opcode;
-    int field1;
-    int field2;
-
-    // opcode
-    while(!Serial.available());
-    opcode = Serial.read();
-    
-    // field 1 most significant byte
-    while(!Serial.available());
-    field1 = Serial.read();
-    field1 <<= 8;
-
-    // field 1 least significant byte
-    while(!Serial.available());
-    field1 += Serial.read();
-
-    // field 2 most significant byte
-    while(!Serial.available());
-    field2 = Serial.read();
-    field2 <<= 8;
-
-    // field 2 least significant byte
-    while(!Serial.available());
-    field2 += Serial.read();
-    
-    // move 
-    if(opcode){
-      int x = (field1) % X_MAX; // 100 um
-      int y = (field2) % Y_MAX; // 100 um
-
-      // center
-      x = (x - X_MAX/2) * STEPS_PER_100_UM;
-      y = (y - Y_MAX/2) * STEPS_PER_100_UM;
-
-      movePenTo(x,y);
-    }
-    
-    // pen
-    else{
-      if(field2){
-        raisePen();
-      }
-      else{
-        lowerPen();
-      }
+  void raise_pen(){
+    if(!pen_state){
+      pen.write(SERVO_ANGLE_UP);
+      pen_state = HIGH;
+      delayMicroseconds(SERVO_DELAY);
     }
   }
 
-  private:
-  void error(){
-    while(1);
+  void lower_pen(){
+    if(pen_state){
+      pen.write(SERVO_ANGLE_DOWN);
+      pen_state = LOW;
+      delayMicroseconds(SERVO_DELAY);
+    }
+  }
+
+  void parse_serial(){
+
+    // send ready
+    //Serial.write('R');
+    
+    // wait for incoming command 
+    while(!Serial.available());
+
+    // read opcode
+    char opcode = Serial.read();
+    
+    switch(opcode){
+      case 'M':
+        parse_move();
+        break;
+      case 'P':
+        parse_pen();
+        break;
+    }
+  }
+
+  void parse_move(){
+
+    // read arguments
+    float x_mm_dest = Serial.parseFloat();
+    float y_mm_dest = Serial.parseFloat();
+
+    // convert to steps
+    int x_dest = x_mm_dest * STEPS_PER_MM;
+    int y_dest = y_mm_dest * STEPS_PER_MM;
+
+    // limit check
+    x_dest = min(X_LIM_STEPS, max(x_dest, 0));
+    y_dest = min(Y_LIM_STEPS, max(y_dest, 0));
+
+    // move 
+    move_to(x_dest,y_dest); 
+         
+  }
+
+  void parse_pen(){
+
+    // read argument
+    int pen_state_dest= Serial.parseInt();
+
+    if(pen_state_dest){
+      raise_pen();
+    }
+    else{
+      lower_pen();
+    }
   }
   
-  private:  
+private:
   Servo pen;
-  AccelStepper xMotor;
-  AccelStepper yMotor;
+  AccelStepper motor_a;
+  AccelStepper motor_b;
   MultiStepper motors;
-  bool penState; // track pen state for some optimizations
+  int pen_state;
 };
 
+void pin_setup(){
+  pinMode(MOTOR_A_STEP_PIN, OUTPUT);
+  pinMode(MOTOR_A_DIR_PIN, OUTPUT);
+  pinMode(MOTOR_A_MS_1_PIN, OUTPUT);
+  pinMode(MOTOR_A_MS_2_PIN, OUTPUT);
+
+  pinMode(MOTOR_B_STEP_PIN, OUTPUT);
+  pinMode(MOTOR_B_DIR_PIN, OUTPUT);
+  pinMode(MOTOR_B_MS_1_PIN, OUTPUT);
+  pinMode(MOTOR_B_MS_2_PIN, OUTPUT);
+
+  pinMode(SERVO_PIN, OUTPUT);
+}
+
 void setup(){
-  // serial setup
   Serial.begin(115200);
 
-  // instantiate
+  pin_setup();
+
   Fern f;
 
   while(1){
-    f.parseSerial();
+    f.parse_serial();
   }
-
 }
 
 void loop() {
- 
+  
 }
